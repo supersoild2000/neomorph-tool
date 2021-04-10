a8bbc8cc2ed4dabdc2b0384fc630cd183d5c0385
Hryhorii Pazych <supersoild2000@gmail.com>
2021-04-10 22:52:05 +0300

    Create basic layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from "react";
 
 function App() {
+  const [width, setWidth] = useState(0);
+  const widthInputRef = useRef<HTMLInputElement>(null);
+  const previewElemStyle = {
+    width: width,
+  };
+  useEffect(() => {
+    if (widthInputRef.current !== null) {
+      setWidth(+widthInputRef.current.value);
+    }
+  }, []);
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <header>
+        <h1>neomorph-tool</h1>
+      </header>
+      <main>
+        <div className="preview-container">
+          <div className="preview-elem" style={previewElemStyle}>
+            <p>Sample text</p>
+          </div>
+        </div>
+        <div className="control-container">
+          <div>
+            <label htmlFor="width">Enter width:</label>
+            <input
+              type="range"
+              name="width"
+              id="width"
+              min="100"
+              max="500"
+              ref={widthInputRef}
+              onChange={(e) => {
+                setWidth(+e.target.value);
+              }}
+            />
+            <span>{width}</span>
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
 
